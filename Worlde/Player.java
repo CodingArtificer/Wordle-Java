c4294a708b0eafd365d7cb3913da43a94c81908f
CodingArtificer <122925512+CodingArtificer@users.noreply.github.com>
2023-11-15 14:33:37 -0600

    Added Player.java

---
+++ Worlde/Player.java
@@ -0,0 +1,32 @@
+
+public class Player 
+{
+    public int tries;
+    public String word;
+
+    public Player(int t, String w)
+    {
+        this.tries = t;
+        this.word = w;
+    }
+    // Gets the number of tries from the Player object.
+    public int getTries()
+    {
+        return tries;
+    }
+    // Sets the amount of tries for the Player object.
+    public void setTries(int num)
+    {
+        tries = num;
+    }
+    // Gets the word for the Player object.
+    public String getWord()
+    {
+        return word;
+    }
+    // Sets the word for the Player object.
+    public void setWord(String w)
+    {
+        word = w;
+    }
+}
